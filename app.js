da5cd464d0dc83da82b617150d4fdef7c6c08735
Laszlo Takacs <laszlo@letmewebyou.com>
2019-11-28 11:40:24 +0100

    Starting files

---
+++ app.js
@@ -0,0 +1,102 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10; // Salting rounds for bcrypt
+
+
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+//POST REQUESTS
+
+app.post("/register", function (req, res) {
+
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+
+  });
+
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({
+      email: username
+    },
+    function (err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          bcrypt.compare(password, foundUser.password, function (err, result) {
+            if (result === true) {
+              res.render("secrets");
+            }
+          });
+        }
+      }
+    }
+  );
+});
+
+app.get("/logout", function (req, res) {
+  res.redirect("/");
+});
+
+/////////////////////////// API GET CALL TO SEE USERS //////////////////////////////////////////////
+app.get("/getusers", function (req, res) {
+  User.find(function (err, foundUsers) {
+    res.send(foundUsers);
+  });
+});
+
+app.listen(3000, function () {
+  console.log("App has been started");
+});
