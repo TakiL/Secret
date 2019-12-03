e0f42f052ee4c73b345109e5d2e3bb6ed38f0b3e
Laszlo Takacs <laszlo@letmewebyou.com>
2019-12-03 14:12:38 +0100

    Further improvement

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const oauth = require("passport-google-oauth20");
+
 const bcrypt = require("bcryptjs");
 const saltRounds = 10; // Salting rounds for bcrypt
-
 
 const app = express();
 app.use(express.static("public"));
@@ -17,86 +21,148 @@ app.use(
   })
 );
 
+//Session initialized
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+//Passport initialized
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+mongoose.set("useCreateIndex", true);
 
+// Mongoose model + passport
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
 
-app.get("/", function (req, res) {
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Reqguests GET
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function (req, res) {
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register", function (req, res) {
+app.get("/register", function(req, res) {
   res.render("register");
+});
+
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
 //POST REQUESTS
 
-app.post("/register", function (req, res) {
-
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-
-    newUser.save(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-
-  });
-
-});
-
-app.post("/login", function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({
-      email: username
-    },
-    function (err, foundUser) {
-      if (err) {
-        console.log(err);
-      } else {
-        if (foundUser) {
-          bcrypt.compare(password, foundUser.password, function (err, result) {
-            if (result === true) {
-              res.render("secrets");
-            }
-          });
-        }
-      }
+//Register using mongoose-passport-local
+app.post("/register", function(req, res) {
+  User.register({ username: req.body.username }, req.body.password, function(
+    err,
+    user
+  ) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
     }
-  );
+  });
 });
 
-app.get("/logout", function (req, res) {
-  res.redirect("/");
+app.post("/login", (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
+  });
 });
+
+// app.post("/register", function(req, res) {
+//   bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+//     const newUser = new User({
+//       email: req.body.username,
+//       password: hash
+//     });
+
+//     newUser.save(function(err) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.render("secrets");
+//       }
+//     });
+//   });
+// });
+
+// app.post("/login", function(req, res) {
+//   const username = req.body.username;
+//   const password = req.body.password;
+
+//   User.findOne(
+//     {
+//       email: username
+//     },
+//     function(err, foundUser) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         if (foundUser) {
+//           bcrypt.compare(password, foundUser.password, function(err, result) {
+//             if (result === true) {
+//               res.render("secrets");
+//             }
+//           });
+//         }
+//       }
+//     }
+//   );
+// });
 
 /////////////////////////// API GET CALL TO SEE USERS //////////////////////////////////////////////
-app.get("/getusers", function (req, res) {
-  User.find(function (err, foundUsers) {
+app.get("/getusers", function(req, res) {
+  User.find(function(err, foundUsers) {
     res.send(foundUsers);
   });
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log("App has been started");
 });
